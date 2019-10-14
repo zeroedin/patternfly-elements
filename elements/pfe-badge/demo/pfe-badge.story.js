3d2d39dd7f89b23fa3ed56ddf726d0a83f95919a
Ivana Rodriguez <ivrodrig@redhat.com>
2019-10-14 15:18:55 -0500

    Initial pfe-badge commit

---
+++ elements/pfe-badge/demo/pfe-badge.story.js
@@ -0,0 +1,11 @@
+import { storiesOf } from "@storybook/polymer";
+import "../pfe-badge";
+
+storiesOf("Badge", module).add(
+  "pfe-badge",
+  () => `
+  <pfe-badge>
+    Badge
+  </pfe-badge>
+  `
+);
